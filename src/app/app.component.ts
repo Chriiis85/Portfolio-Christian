9952663e2320ffbc6afdff933af0021689890a28
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-09-05 13:15:49 +0200

    ContainersCreados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { AboutPageComponent } from "./about-page/about-page.component";
+import { ProjectsPageComponent } from "./projects-page/projects-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPageComponent, AboutPageComponent, ProjectsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
